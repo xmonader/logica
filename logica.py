a8d7462e4b81641a16cce1a3a0db517f534a30db
xmonader <xmonader@gmail.com>
2018-11-09 20:14:09 +0200

    initial working version

--- logica.py
+++ logica.py
@@ -61,12 +61,7 @@ def test_dontunify_complex():
     l2 = [ '?j', 11]
     print(unify(l1, l2))
 
-def query_simple(query, facts):
-    for f in facts:
-        res = unify(query, f)
-        if res:
-            print(res)
-        
+
 def rename_vars(clause, env):
     # print("RENAMING .... ENV: ", env)
     newclause = clause[:]
@@ -78,59 +73,70 @@ def rename_vars(clause, env):
     return newclause
 
 
-def ask(facts, clauses, env=None, depth=0, visited=None, msg=""):
-    import copy
-    
-    env = copy.deepcopy(env) or {}
-    visited = visited or {'facts':[], 'clauses':[]}
-    # print(" "*depth, "FACTS: ", facts, "CLAUSES: ", clauses)
-    # print(" "*depth, "ENV: ", env, " DEPTH:", depth)
-    # print("VISITED: ", visited)
-    if len(visited['clauses']) == len(clauses):
-        # print("SUCCESS")
-        return env
-    if not clauses:
-        # print(" Failed with env {}".format(env))
-        return env 
-
-
+def query(facts, clauses, env=None):
     results = []
-    # for fid, fact in enumerate(facts):
-    for cid, clause in enumerate(clauses):
-        choices = []
-        for f in facts:
-            if f[0] != clause[0]:
-                continue
-            res = unify(f, clause, env)
-            if res:
-                choices.append(res)
-                # print("Clause {} can be unified with {} in ways {}".format(clause, f, choices))
+    def ask(facts, clauses, env=None, depth=0, originalclauseslen=-1):
+        import copy
+        
+        if originalclauseslen == -1:
+            originalclauseslen = len(clauses)
 
-        for e in choices:
-            rewritten_clauses = []
-            for c in clauses:
-                rewritten_clauses.append(rename_vars(c, e)) 
-            for rcid, rc in enumerate(rewritten_clauses):
-                for fid, fact in enumerate(facts):
-                    # if fact[0] != rc[0]:
-                    #     continue
-                    res = unify(fact, clause, e)
-                    if res:
-                        # print("[+]unified {} and fact {}".format(rc, fact))
-                        newvisited=  copy.deepcopy(visited)
-                        newvisited['facts'].append(fact)
-                        newvisited['clauses'].append(clause)
-                        r = ask(facts, rewritten_clauses[rcid+1:], res, rcid, newvisited)
-                        print("R: ", r, depth)
-                        if depth == len(clauses):
-                            results.append(r) 
+        # for fid, fact in enumerate(facts):
+        clausesinfo = {}
+        for cid, clause in enumerate(clauses):
+            choices = []
+            for f in facts:
+                if f[0] != clause[0]:
+                    continue
+                newenv = copy.deepcopy(env)
+                res = unify(f, clause, newenv)
+                if res:
+                    choices.append(res)
+                    # print("Clause {} can be unified with {} in ways {}".format(clause, f, choices))
+            clausesinfo[cid] = {'choices': choices, 'clause':clause}
+        # print(clausesinfo)
+        for fid, fact in enumerate(facts):
+            for clauseid, clauseinfo in clausesinfo.items():
+                choices = clauseinfo['choices']
+                clause = clauseinfo['clause']
+                for e in choices:
+                    rewritten_clause = rename_vars(clause, e)
+                    res = unify(fact, rewritten_clause, e)
+                    if not res:
+                        continue
+                        # print("[-] couldn't unify {} with {} in env {}".format(fact, rewritten_clause, e))
+                    else:
+                        # print("     "*depth, "[+] unified {} with {} in env {}".format(fact, rewritten_clause, res))
+                        res = ask(facts[fid+1:], clauses[clauseid+1:], res, depth+1, originalclauseslen)
+                        if depth + 1 == originalclauseslen:
+                            # print("SUCCESS!!!")
+                            # print(res)
+                            results.append(res)
+                            # return res
+        return env
+    ask(facts, clauses, env, 0, -1)
     return results
+
+
+
+def test_query_simple():
+    facts = [ 
+        ["man", "ahmed"],
+        ["man", "jo"],
+        ["man", "prince"],
+    ]
+
+    clauses = [["man", "?name"],]
+    print(query(facts, clauses))
+
+
 
 def test_query_complex():
     facts = [ 
         ["woman", "nour"],
         ["man", "jo"],
         ["man", "ahmed"],
+        ["loves", "nour", "python"],
         ["loves", "ahmed", "python"],
         ["loves", "jo", "gevent"],
     ]
@@ -138,12 +144,10 @@ def test_query_complex():
         ["man", "?name"],
         ["loves", "?name", "python"],
     ]
-
     clauses2 = [
         ["man", "?name"],
         ["loves", "?name", "gevent"],
     ]
-
     clauses3 = [
         ["woman", "?name"],
     ]
@@ -153,31 +157,23 @@ def test_query_complex():
     clauses5 = [
         ["man", "?name"],
     ]
-    for c in [clauses1, clauses2, clauses3, clauses4, clauses5]:
-        print("Query: ", c)
-        print(ask(facts, c))
-
-
-def test_simple_facts():
-    facts = [ 
-        ["man", "ahmed"],
-        ["man", "jo"],
-        ["man", "prince"],
+    clauses6 = [
+        ["loves", "?name", "python"]
     ]
-
-    q = ["man", "?name"]
-    query_simple(q, facts)
+    for c in [clauses1, clauses2, clauses3, clauses4, clauses5, clauses6]:
+        print("Query: ", c)
+        print(query(facts, c))
 
 
 
 
 def main():
-    # test_unify_simple()
-    # test_unify_simple_with_env()
-    # # test_unify_complex()
-    # # test_unify_very_complex()
-    # # test_dontunify_complex()
-    # test_simple_facts()
+    test_unify_simple()
+    test_unify_simple_with_env()
+    test_unify_complex()
+    test_unify_very_complex()
+    test_dontunify_complex()
+    test_query_simple()
     test_query_complex()
 
 if __name__ == '__main__':
